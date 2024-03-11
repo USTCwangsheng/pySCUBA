dcec24346bc2a60de6a8ff15be14549c6852b3db
USTCwangsheng <162691210+USTCwangsheng@users.noreply.github.com>
2024-03-11 17:01:34 +0800

    Update SCUBAsketch.py

--- demo/SCUBASketch/SCUBAsketch.py
+++ demo/SCUBASketch/SCUBAsketch.py
@@ -4,7 +4,7 @@ from pySCUBA import Sketchbuildpdb
 #################sketch生成初始结构#####################
 sketchpar='sketch.par'#Parameter file, which is automatically generated if it does not exist
 skecthfile='3pvh.txt'       #sketch coordinate file. Must exist.
-linkoption=1                #Connect loop option. 0 is not connected and 1 is connected.
+linkoption=1                #Link loop option. 0 is not connected and 1 is connected.
 randomseed=15               
 outputname='build'   #Output the file name of the pdb with this prefix.
 gennumber=2                 #Number of structures generated
